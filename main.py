2d7822968279fd0bbb6d0b1e9c1612bb76991bef
1HailStorm1 <70704650+1HailStorm1@users.noreply.github.com>
2022-07-30 23:45:18 +1000

    initial commit

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ print(os.getcwd())
 print(os.listdir())
 print(os.listdir('cycleGAN'))
 if not os.path.exists("cycleGAN/datasets/user_photos"):
-    subprocess.call('git submodule add https://github.com/nazar-karpov/pytorch-CycleGAN-and-pix2pix cycleGAN', shell=True)
     os.mkdir('cycleGAN/datasets/user_photos')
     os.mkdir('cycleGAN/datasets/user_photos/testA')
     # os.chdir('./cycleGAN')
